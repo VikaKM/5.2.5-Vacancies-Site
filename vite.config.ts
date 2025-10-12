123f37a51cd7f94981e8a684000ebdb4104133df
Виктория Ковалевская <victoriakm25@gmail.com>
2025-10-12 21:34:28 +0300

    Deploy updated site

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/4.2.3-Vacancies-Site/', 
   test: {
     globals: true,          
     environment: 'jsdom',
